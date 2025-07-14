3e6ec7922ce6c646456eff48998282c8a3c8d628
Mahamedg1424 <144610735+Mahamedg1424@users.noreply.github.com>
2025-07-14 07:02:28 +0300

    Update tokens.js

--- tokens.js
+++ tokens.js
@@ -4,9 +4,9 @@ export default [
 
     {
 
-        channelId: "1344059457747026005",
+        channelId: "1375829437270200427",
 
-        serverId: "1344059457046319198",
+        serverId: "1375829435684753543",
 
         token: process.env.token1,
 
